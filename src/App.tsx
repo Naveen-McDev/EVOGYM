0e5353841812c6adb2ea31affa3efc51a05351a2
Naveen Krish <98112231+Naveen-McDev@users.noreply.github.com>
2023-01-14 13:52:44 +0530

    contact us page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/scenes/navbar";
 import { useEffect, useState } from "react";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
 import { SelectedPage } from "./shared/type";
 import OurClasses from "./scenes/ourClasses";
 
@@ -38,6 +39,7 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </div>
   );
 }
